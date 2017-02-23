59fc1ccb9a533e80c4cd47bc4653027fb40c4c8b
tracer-ics2015 <tracer@njuics.org>
2017-02-23 23:04:06 +0800

    >  compile NEMU 161520328 wangxin Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  23:04:06 up  1:00,  1 user,  load average: 0.00, 0.00, 0.00 bcd7a40f5f16cdea4bfce2d99d6820e68ce32d7e

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -92,12 +92,14 @@ static int cmd_x(char *args) {
 	char *arg2 = strtok(NULL, " ");
 	uint32_t p ;
 	sscanf(arg2,"%x" ,&p);
-	int i;
+	int i,j;
 	for(i=1 ; i<=n ; i++)
 	{
 		printf("%x : ",p);
-		printf("%2x  ",swaddr_read(p,4));
-		p+=4;
+		for(j=0;j<=4;j++,p++)
+		{
+		printf("%2x\t",swaddr_read(p,1));
+		}
 		printf("\n");
 	}
 	return 0;
