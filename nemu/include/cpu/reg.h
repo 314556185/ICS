c7fb4ded79420158a83c027e4e925160fecbd881
tracer-ics2015 <tracer@njuics.org>
2017-03-29 00:51:29 +0800

    >  compile NEMU 161520328 wangxin Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  00:51:29 up  2:40,  1 user,  load average: 0.09, 0.04, 0.01 1ad20b0a9096dba22e3008a1f8a6b9bdadbf234

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,8 +14,10 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef union {
-	union {
+typedef struct {
+	union 
+	{
+		union  {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
@@ -25,7 +27,29 @@ typedef union {
 	struct {
 		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 		swaddr_t eip;
+			};
 	};
+	struct {
+		uint32_t value;
+		uint32_t CF : 1 ;
+		uint32_t : 1 ;
+		uint32_t PF : 1 ;
+		uint32_t : 1 ;
+		uint32_t AF : 1 ;
+		uint32_t : 1 ;
+		uint32_t ZF : 1 ;
+		uint32_t SF : 1 ;
+		uint32_t TF : 1 ;
+		uint32_t IF : 1 ;
+		uint32_t DF : 1 ;
+		uint32_t OF : 1 ;
+		uint32_t OLIP : 2 ;
+		uint32_t NT : 1 ;
+		uint32_t : 1 ;
+		uint32_t RF : 1 ;
+		uint32_t VM : 1 ;
+		uint32_t : 14 ;
+	}eflags;
 } CPU_state;
 
 extern CPU_state cpu;
---
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -0,0 +1,38 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sub
+
+static void do_execute() {
+	DATA_TYPE rval = -op_src->val;
+	DATA_TYPE result = op_dest->val + rval;
+	OPERAND_W(op_dest, result);
+
+	// OF, SF, ZF, CF, PF
+	uint32_t step = 0x8 * DATA_BYTE - 1;
+	unsigned sf = 0x1 & (op_src->val >> step);
+	unsigned df = 0x1 & (op_dest->val >> step);
+	unsigned rf = 0x1 & (result >>step);
+
+	cpu.eflags.OF = (!df && sf && rf) || (df && !sf && !rf);
+	cpu.eflags.SF = rf;
+	cpu.eflags.ZF = (result == 0);
+	cpu.eflags.CF = (uint32_t) (op_dest->val) < (uint32_t)(op_src->val);
+
+	result = 0xff & result;
+	unsigned count;
+	for(count = 0;result;++count)
+		result &= (result - 1);
+	cpu.eflags.PF = !(count % 2);
+
+	print_asm_template2();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
