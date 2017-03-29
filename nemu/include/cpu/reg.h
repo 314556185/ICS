967c35712672ab07351cd0de38c0aa952f3dc56f
tracer-ics2015 <tracer@njuics.org>
2017-03-29 10:38:25 +0800

    >  compile NEMU 161520328 wangxin Linux debian 3.16.0-4-586 #1 Debian 3.16.39-1 (2016-12-30) i686 GNU/Linux  10:38:25 up 16 min,  1 user,  load average: 0.09, 0.06, 0.07 10190b7e955b5d5a4c957585887f43f10372b2

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -17,39 +17,33 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 typedef struct {
 	union 
 	{
-		union  {
+		union {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
 	} gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
-	struct {
-		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-		swaddr_t eip;
-			};
+		struct {
+			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+			swaddr_t eip;
+		};
 	};
+
 	struct {
-		uint32_t value;
-		uint32_t CF : 1 ;
-		uint32_t : 1 ;
-		uint32_t PF : 1 ;
-		uint32_t : 1 ;
-		uint32_t AF : 1 ;
-		uint32_t : 1 ;
-		uint32_t ZF : 1 ;
-		uint32_t SF : 1 ;
-		uint32_t TF : 1 ;
-		uint32_t IF : 1 ;
-		uint32_t DF : 1 ;
-		uint32_t OF : 1 ;
-		uint32_t OLIP : 2 ;
-		uint32_t NT : 1 ;
-		uint32_t : 1 ;
-		uint32_t RF : 1 ;
-		uint32_t VM : 1 ;
-		uint32_t : 14 ;
+		unsigned CF: 1;
+		unsigned PAD1: 1;
+		unsigned PF: 1;
+		unsigned PAD2: 3;
+		unsigned ZF: 1;
+		unsigned SF: 1;
+		unsigned PAD3: 1;
+		unsigned IF: 1;
+		unsigned DF: 1;
+		unsigned OF: 1;
+		unsigned PAD4: 20;
 	}eflags;
+
 } CPU_state;
 
 extern CPU_state cpu;
